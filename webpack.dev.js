9bf6751d09ae8bf815d7c4cb6823a8f28153a8c9
fengfanliu <1144357194@qq.com>
2026-01-09 18:12:03 +0800

    Configure dev server host and port for external access

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,8 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
+        host: '0.0.0.0',
+        port: 8080,
         // progress: true,
         liveReload: false
     },
